e40f4d1789aa2a55f1d89419c7e6dddddaaa146a
JoaoAlexandre <alexandremjoao@gmail.com>
2024-06-26 17:47:51 -0300

    feat: "add User"

--- src/main/java/com/joaoalexandre/spring_security/websecurity/WebSecurityConfig.java
+++ src/main/java/com/joaoalexandre/spring_security/websecurity/WebSecurityConfig.java
@@ -7,17 +7,18 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.authorizeRequests(requests -> requests
                 .dispatcherTypeMatchers(HttpMethod.valueOf("/")).permitAll()
                 .dispatcherTypeMatchers(HttpMethod.POST, DispatcherType.valueOf("/login")).permitAll()
                 .dispatcherTypeMatchers(HttpMethod.valueOf("/managers")).hasAnyRole("MANAGERS")
-                .dispatcherTypeMatchers(HttpMethod.valueOf("/users")).hasAnyRole("USERS","MANAGERS")
-                .anyRequest().authenticated();
+                .dispatcherTypeMatchers(HttpMethod.valueOf("/users")).hasAnyRole("USERS", "MANAGERS")
+                .anyRequest().authenticated());
     }
 
 }
